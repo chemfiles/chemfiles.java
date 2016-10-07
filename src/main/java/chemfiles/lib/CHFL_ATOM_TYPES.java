e1f7b0ea2ae51113f8d5e03d0c2d6482aeb7dd81
Luthaf <luthaf@luthaf.fr>
2016-10-07 23:53:26 +0200

    Wrap enums to JNA

---
+++ src/main/java/chemfiles/lib/CHFL_ATOM_TYPES.java
@@ -0,0 +1,24 @@
+// Chemfiles, an efficient IO library for chemistry file formats
+// Copyright (C) 2015 Guillaume Fraux
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+/* ========================================================================= */
+// !!!! AUTO-GENERATED FILE !!!! Do not edit. See the bindgen repository for
+// the generation code (https://github.com/chemfiles/bindgen).
+// This file contains Java JNA interface to the C API
+/* ========================================================================= */
+
+package chemfiles.lib;
+
+import com.sun.jna.*;
+import com.sun.jna.ptr.*;
+
+public interface CHFL_ATOM_TYPES {
+    public static int CHFL_ATOM_ELEMENT = 0;
+    public static int CHFL_ATOM_COARSE_GRAINED = 1;
+    public static int CHFL_ATOM_DUMMY = 2;
+    public static int CHFL_ATOM_UNDEFINED = 3;
+}
---
+++ src/main/java/chemfiles/lib/CHFL_CELL_TYPES.java
@@ -0,0 +1,23 @@
+// Chemfiles, an efficient IO library for chemistry file formats
+// Copyright (C) 2015 Guillaume Fraux
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+/* ========================================================================= */
+// !!!! AUTO-GENERATED FILE !!!! Do not edit. See the bindgen repository for
+// the generation code (https://github.com/chemfiles/bindgen).
+// This file contains Java JNA interface to the C API
+/* ========================================================================= */
+
+package chemfiles.lib;
+
+import com.sun.jna.*;
+import com.sun.jna.ptr.*;
+
+public interface CHFL_CELL_TYPES {
+    public static int CHFL_CELL_ORTHORHOMBIC = 0;
+    public static int CHFL_CELL_TRICLINIC = 1;
+    public static int CHFL_CELL_INFINITE = 2;
+}
